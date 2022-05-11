e260880297c8681aad40b3c534a1c43d2811d52e
azemmath <77932091+azemmath@users.noreply.github.com>
2022-05-11 12:33:25 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  if (is.matrix(x)) {
+    if (nrow(x)==ncol(x)){
+      invm <- NULL
+      set <- function(y) {
+        x <<- y
+        invm <<- NULL
+      }
+      get <- function() x
+      setinv <- function(invm) invm <<- solve(x)
+      getinv <- function() invm
+      list(set = set, get = get,setinv = setinv,getinv = getinv)}
+    else
+      message('not a square matrix')}
+  else
+    message('not a matrix')
 }
 
 
@@ -12,4 +26,21 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m1=x$get()
+  if (is.matrix(m1)) {
+    if (nrow(m1)==ncol(m1)){
+      invm <- x$getinv()
+      if(!is.null(invm)) {
+        message("getting cached data")
+        return(invm)
+      }
+      data <- x$get()
+      invm <- solve(data)
+      x$setinv(invm)
+      invm
+    }
+    else
+      message('not a square matrix')}
+  else
+    message('not a matrix')
 }
